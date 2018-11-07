56be5b5efcefad9ee7df05b10e366d899db78ec9
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-11-07 23:04:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.Factory.StartNew(speak);
+            //Task t= Task.Run(new Action(speak));
+           // t.Start();
+            Console.WriteLine("Waiting for completion");
+            t.Wait();
+            Console.WriteLine("All Done");
+
+            Console.ReadLine();
+        }
+        private static void speak()
+        {
+            Console.WriteLine("Hello World");
+            // Console.ReadLine();
+        }
+    }
+}
